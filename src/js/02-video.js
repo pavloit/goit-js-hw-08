66bb5a5a46a5e48371d5d8fbfa4a3772b9693f99
pavloit <dba.devop@gmail.com>
2023-10-07 10:17:42 +0300

    task2-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector("#vimeo-player");
+const player = new Player(iframe, {
+    id: "vimeo-player",
+    width: 1280
+});
+
+const storedTime = localStorage.getItem('videoplayer-current-time')
+let currentTime = !!storedTime ? storedTime : 0; 
+player.setCurrentTime(currentTime)
+player.on('timeupdate', throttle(storeTime, 1000))
+
+function storeTime(event) {
+    const time = event.seconds;
+    localStorage.setItem('videoplayer-current-time', time);
+}
