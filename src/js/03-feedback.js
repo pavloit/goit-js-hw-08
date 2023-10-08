2bc54dfa3e6a590e17d31e35752364acf655a6a7
pavloit <dba.devop@gmail.com>
2023-10-08 15:23:25 +0300

    task03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(".feedback-form");
+const storedData = localStorage.getItem('feedback-form-state');
+const emailInput = document.querySelector('input[name="email"]') 
+const messageInput = document.querySelector('textarea[name="message"]')
+let feedbackForm = {
+    email: "",
+    message: "",
+}
+if (!!storedData) {
+    feedbackForm = JSON.parse(storedData)
+}
+
+emailInput.value = feedbackForm.email;
+messageInput.value = feedbackForm.message;
+
+
+form.addEventListener('input', throttle(onInput, 500))
+
+function onInput(event) {
+    const target = event.target;
+    feedbackForm[target.name] = target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackForm));
+}
+
+function clearForm() {
+    emailInput.value = "";
+    messageInput.value = "";
+}
+    
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(event) {
+    event.preventDefault();
+    localStorage.clear();
+    clearForm();
+    console.log(feedbackForm);
+}
