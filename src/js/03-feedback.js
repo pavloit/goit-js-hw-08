798f4ca82d969d95360aa0c28c0f75c99c028dd4
pavloit <dba.devop@gmail.com>
2023-10-09 12:02:40 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,9 @@
 import throttle from "lodash.throttle";
-
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector(".feedback-form");
 const emailInput = document.querySelector('input[name="email"]');
 const messageInput = document.querySelector('textarea[name="message"]');
-const storedData = localStorage.getItem('feedback-form-state');
+const storedData = localStorage.getItem(STORAGE_KEY);
 const feedbackForm = JSON.parse(storedData) ?? {
     email: "",
     message: "",
@@ -18,7 +18,7 @@ form.addEventListener('input', throttle(onInput, 500))
 function onInput(event) {
     const target = event.target;
     feedbackForm[target.name] = target.value;
-    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackForm));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
 }
 
 function clearForm() {
@@ -36,6 +36,6 @@ function onSubmit(event) {
         return alert("Обидва поля мають бути заповнені 😝")    
     }
     console.log(feedbackForm);
-    localStorage.removeItem('feedback-form-state');
+    localStorage.removeItem(STORAGE_KEY);
     clearForm();   
 }
